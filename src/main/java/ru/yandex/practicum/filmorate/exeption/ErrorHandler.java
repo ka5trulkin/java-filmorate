c797dbb67b1fe4f96872c1f53b3861e4eaa887f0
ka5trulkin <ka5trulkin@yandex.by>
2023-03-08 14:19:38 +0300

    feat: исправить FilmController

---
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/ErrorHandler.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.exeption;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseError dataAlreadyExist(RequestException exception) {
+        log.error(exception.getMessage(), exception);
+        return new ResponseError(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoDataException.class)
+    public ResponseError noData(NoDataException exception) {
+        log.error(exception.getMessage(), exception);
+        return new ResponseError(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
