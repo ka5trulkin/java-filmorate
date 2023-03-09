878d6a92be7b128f69baf9759174addbdd264a1c
ka5trulkin <ka5trulkin@yandex.by>
2023-03-09 17:22:44 +0300

    fix: убрать @Entity

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,20 +5,16 @@ import lombok.Data;
 import lombok.NonNull;
 import ru.yandex.practicum.filmorate.validation.AfterFirstFilm;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
-@Entity
 @Data
 @Builder(toBuilder = true)
 @NonNull
 public class Film implements IdHolder {
-    @Id
     private int id;
     @NotBlank
     private String name;
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,19 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.validation.NotContainSpace;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
-@Entity
 @Data
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class User implements IdHolder{
-    @Id
     private int id;
     @Email
     private String email;
